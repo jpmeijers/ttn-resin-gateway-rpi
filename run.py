d9e7791e39c3eb85215c2c09b409af5fc8235b67
Thomas Walther <tho.walther@gmail.com>
2019-03-22 23:41:03 +0100

    Update run.py
    Cellular wait error fixed.

--- run.py
+++ run.py
@@ -27,8 +27,8 @@ if os.environ.get('HALT') != None:
   print ("*** HALT asserted - exiting ***")
   sys.exit(0)
   
- if os.environ.get("CELLULAR_WAIT") != None:
-  cellular_wait_s = os.environ.get("CELLULAR_WAIT")
+ if os.environ.get('CELLULAR_WAIT') != None:
+  cellular_wait_s = os.environ.get('CELLULAR_WAIT')
   print ("*** Wait time for cellular connnection in s: "+str(os.environ.get('CELLULAR_WAIT')))
   sleep(cellular_wait_s)
 
